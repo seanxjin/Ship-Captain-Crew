460388abd8450c67e0d4527615e86bbf34aaaefb
Sean <s.jin8@share.epsb.ca>
2023-11-30 13:48:42 -0700

    Started

--- Multi-day Project Ship, Captain, Crew/player.py
+++ Multi-day Project Ship, Captain, Crew/player.py
@@ -4,27 +4,73 @@ author: Sean Jin
 date-created: 2023-11-28
 '''
 
-import dice
+from dice import Dice
 class Player:
 
     # Constructor
-    def __init__(self, NAME, HAND):
+    def __init__(self):
         """
         Player class in the program
         :param NAME: str
         :param HAND: list -> int
         """
-        self.__NAME = NAME
-        self.__HAND = HAND
+        self.__NAME = input("What is your name? ")
+        self.__UNFOUNDDIE = [Dice(6).getDiceValue(), Dice(6).getDiceValue(), Dice(6).getDiceValue(), Dice(6).getDiceValue(), Dice(6).getDiceValue()]
+        self.__FOUNDDIE = []
+        self.__SCORE = 0
 
     # MODIFIER method
+    def rollDice(self):
+        """
+        Rolls the dice in the hand
+        :return: none
+        """
+        for die in self.__UNFOUNDDIE:
+            die.rollDie()
+    def addScore(self, GAINS):
+        """
+        Adds score to the Player
+        :return: none
+        """
+        self.__SCORE = self.__SCORE + GAINS
+
+    def checkFoundDie(self):
+        """
+        Checks first if ship is found, then captain, then crew
+        :return: none
+        """
+        SORTED_LIST = []
 
     # ACCESSOR method
     def getHand(self):
         """
-        Gets the hand of the dice
+        prints the hand that the user has
         :return: list
         """
-        return self.__HAND
+        DISPLAY_HAND = []
+        for i in range(len(self.__UNFOUNDDIE)):
+            DISPLAY_HAND.append(self.__UNFOUNDDIE[i].getDiceValue())
+        return DISPLAY_HAND
+
+    def getScore(self):
+        """
+        prints the score
+        :return: int
+        """
+        return self.__SCORE
+
+    def getName(self):
+        """
+        Gets the name of the player
+        :return: str
+        """
+        return self.__NAME
+
+
+if __name__ == "__main__":
+    PLAYER = Player()
+    PLAYER.rollDice()
+    PLAYER.checkFoundDie()
+
 
 
